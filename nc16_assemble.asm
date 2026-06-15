68f8d0b5291f662fe428d542f8b40c662737e539
KTERAPO\gapck <gameblog60@gmail.com>
2026-06-15 17:41:03 +0900

    ドキュメントの記載に合わせてnc16_assemble.asmを更新

--- nc16_assemble.asm
+++ nc16_assemble.asm
@@ -30,43 +30,57 @@
     ; --- 算術・論理演算 ---
     add {r1: regs},{r2: regs} => 0x00 @ r1 @ r2 @ 0x0000
     add {r1: regs},{opd:u16}  => 0x00 @ r1 @ 0xf @ opd
-    add {r1: regs},[a]         => 0x00 @ r1 @ 0x7 @ 0x0000
+    add {r1: regs},[memaddr+{opd:u16}]=> 0x00 @ r1 @ 0x9 @ opd
 
     sub {r1: regs},{r2: regs} => 0x01 @ r1 @ r2 @ 0x0000
     sub {r1: regs},{opd: u16}  => 0x01 @ r1 @ 0xf @ opd
-    sub {r1: regs},[a]         => 0x01 @ r1 @ 0x7 @ 0x0000
+    sub {r1: regs},[memaddr+{opd:u16}]=> 0x01 @ r1 @ 0x9 @ opd
 
     mul {r1: regs},{r2: regs} => 0x02 @ r1 @ r2 @ 0x0000
     mul {r1: regs},{opd: u16}  => 0x02 @ r1 @ 0xf @ opd
-    mul {r1: regs},[a]         => 0x02 @ r1 @ 0x7 @ 0x0000
+    mul {r1: regs},[memaddr+{opd:u16}]=> 0x02 @ r1 @ 0x9 @ opd
 
     and {r1: regs},{r2: regs} => 0x03 @ r1 @ r2 @ 0x0000
     and {r1: regs},{opd: u16}  => 0x03 @ r1 @ 0xf @ opd
-    and {r1: regs},[a]         => 0x03 @ r1 @ 0x7 @ 0x0000
+    and {r1: regs},[memaddr+{opd:u16}]=> 0x03 @ r1 @ 0x9 @ opd
 
     or  {r1: regs},{r2: regs} => 0x04 @ r1 @ r2 @ 0x0000
     or  {r1: regs},{opd: u16}  => 0x04 @ r1 @ 0xf @ opd
-    or  {r1: regs},[a]         => 0x04 @ r1 @ 0x7 @ 0x0000
+    or  {r1: regs},[memaddr+{opd:u16}]=> 0x04 @ r1 @ 0x9 @ opd
 
     not {r1: regs}             => 0x05 @ r1 @ 0xf @ 0x0000
 
     xor {r1: regs},{r2: regs} => 0x06 @ r1 @ r2 @ 0x0000
     xor {r1: regs},{opd: u16}  => 0x06 @ r1 @ 0xf @ opd
     xor {r1: regs},[a]         => 0x06 @ r1 @ 0x7 @ 0x0000
+    xor {r1: regs},[memaddr+{opd:u16}]=> 0x06 @ r1 @ 0x9 @ opd
 
     shr {r1: regs},{r2: regs} => 0x07 @ r1 @ r2 @ 0x0000
     shr {r1: regs},{opd: u16}  => 0x07 @ r1 @ 0xf @ opd
     shr {r1: regs},[a]         => 0x07 @ r1 @ 0x7 @ 0x0000
+    shr {r1: regs},[memaddr+{opd:u16}]=> 0x07 @ r1 @ 0x9 @ opd
 
     ; --- データ転送命令 ---
     mov {r1: regs},{r2: regs} => 0x08 @ r1 @ r2 @ 0x0000
-    mov {r1: regs},[a]         => 0x08 @ r1 @ 0x7 @ 0x0000
+    mov {r1: regs},[memaddr+{opd: u16}] => 0x08 @ r1 @ 0x9 @ opd
+    mov memval,[memaddr+{opd: u16}] => 0x08 @ 0xb @ 0x9 @ opd
     mov {r1: regs},[{opd: u16}] => 0x08 @ r1 @ 0x7 @ opd
     mov {r1: regs},{opd: u16} => 0x08 @ r1 @ 0xf @ opd
     
-    ; メモリ書き込みはBレジスタ固定仕様
-    mov [a],b                 => 0x08 @ 0x7 @ 0x1 @ 0x0000
-    mov [{opd: u16}],b         => 0x08 @ 0x7 @ 0x1 @ opd
+    ; メモリ書き込みはmemaddrレジスタ固定仕様
+    mov memaddr,{r1: regs} => 0x08 @ 0xa @ r1 @ 0x0000
+    mov [memaddr+{opd: u16}],memval => 0x08 @ 0xa @ 0xa @ opd
+    mov [{opd: u16}],memval         => 0x08 @ 0x7 @ 0xa @ opd
+
+    mov {r1: regs},[{r2: regs}+{opd: u16}] => asm{
+        mov memaddr,r2
+        mov r1,[memaddr+opd]
+    }
+    mov [{r1: regs}+{opd: u16}],{r2: regs} => asm{
+        mov memaddr,r1
+        mov memval,r2
+        mov [memaddr+opd],memval
+    }
 
     in  {r1: regs}             => 0x09 @ r1 @ 0xf @ 0x0000
     out {r1: regs}             => 0x0a @ r1 @ 0xf @ 0x0000
@@ -101,6 +115,8 @@
     jl  {r1: regs}             => 0x1a @ r1 @ 0xf @ 0x0000
     jle {r1: regs}             => 0x1b @ r1 @ 0xf @ 0x0000
 
+    jmp memval                 => 0x11 @ 0xb @ 0xf @ 0x0000
+
     ; --- 比較・停止 ---
     cmp {r1: regs},{r2: regs} => 0x12 @ r1 @ r2 @ 0x0000
     cmp {r1: regs},{opd: u16}  => 0x12 @ r1 @ 0xf @ opd
@@ -115,30 +131,36 @@
     ; プログラムを組む際は上記項目に注意すること。
     push {opd: u16} => asm{
         sub sp,1
-        mov b,{opd}
-        mov a,sp
-        mov [a],b
+        mov memval,{opd}
+        mov memaddr,sp
+        mov [memaddr],memval
     }
     push {r1: regs} => asm{
         sub sp,1
-        mov b,{r1}
-        mov a,sp
-        mov [a],b
+        mov memval,{r1}
+        mov memaddr,sp
+        mov [memaddr+0],memval
     }
     pop {r1: regs} => asm{
-        mov a,sp
-        mov {r1},[a]
+        mov memaddr,sp
+        mov {r1},[memaddr+0]
         add sp,1
     }
     call {opd: u16} => asm{
         lpc
-        add a,0xc;push命令、jmp命令の命令長を修正した場合、加算する値は修正する必要がある
-        push a
+        add memval,0xc
+        ; push memval
+        sub sp,1
+        mov memaddr,sp
+        mov [memaddr+0],memval
         jmp {opd}
     }
     ret => asm{
-        pop a
-        jmp a
+        ; pop memval
+        mov memaddr,sp
+        mov memval,[memaddr+0]
+        add sp,1
+        jmp memval
     }
 
 
