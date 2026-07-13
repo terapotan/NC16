4c646f1057d7f2d958efefe0d9d66a97484a02f7
KTERAPO\gapck <gameblog60@gmail.com>
2026-07-13 23:30:39 +0900

    割り込み番号によって処理が分岐していることを確認した

--- main.asm
+++ main.asm
@@ -1,12 +1,18 @@
 #include "nc16_assemble.asm"
 
 interrupt_handler_base_address = 0x5000
-interrupt_number = 1
+interrupt_number_1 = 1
+interrupt_number_2 = 2
+
 mov memaddr,0x0000
 ; 割り込みハンドラを設定
-num = interrupt_handler_base_address+interrupt_number
+num1 = interrupt_handler_base_address+interrupt_number_1
+num2 = interrupt_handler_base_address+interrupt_number_2
 mov memval,int_handler
-mov [memaddr+num],memval
+mov [memaddr+num1],memval
+
+mov memval,int_handler_long
+mov [memaddr+num2],memval
 
 in a
 loop:
@@ -18,4 +24,21 @@ int_handler:
     out 0xffff
     out 0x0000
     out 0xffff
+    intret
+
+int_handler_long:
+    out 0xffff
+    out 0x0000
+    out 0xffff
+    out 0x0000
+    out 0xffff
+    out 0x0000
+    out 0xffff
+    out 0x0000
+    out 0xffff
+    out 0x0000
+    out 0xffff
+    out 0x0000
+    out 0xffff
+    out 0x00ff
     intret
