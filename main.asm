16e423bfe0d5a711b1417cd954e67da5091c142a
KTERAPO\gapck <gameblog60@gmail.com>
2026-07-17 22:26:49 +0900

    echo back(keyboradに入力した文字列がTTY上のそのまま流れる)実装完了、動作チェックも完了

--- main.asm
+++ main.asm
@@ -2,43 +2,19 @@
 
 interrupt_handler_base_address = 0x5000
 interrupt_number_1 = 1
-interrupt_number_2 = 2
 
 mov memaddr,0x0000
 ; 割り込みハンドラを設定
 num1 = interrupt_handler_base_address+interrupt_number_1
-num2 = interrupt_handler_base_address+interrupt_number_2
 mov memval,int_handler
 mov [memaddr+num1],memval
 
-mov memval,int_handler_long
-mov [memaddr+num2],memval
-
-in a
+mov a,0
 loop:
-    out a
     add a,1
     jmp loop
 
 int_handler:
-    out 0xffff
-    out 0x0000
-    out 0xffff
-    intret
-
-int_handler_long:
-    out 0xffff
-    out 0x0000
-    out 0xffff
-    out 0x0000
-    out 0xffff
-    out 0x0000
-    out 0xffff
-    out 0x0000
-    out 0xffff
-    out 0x0000
-    out 0xffff
-    out 0x0000
-    out 0xffff
-    out 0x00ff
+    in b
+    out b
     intret
